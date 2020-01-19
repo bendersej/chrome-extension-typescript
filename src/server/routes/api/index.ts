eed926a14b0335ff882bf305e9ef48bb57efdf4f
Ben <benjamin.andremicolon@gmail.com>
2020-01-19 17:32:18 +0200

    chore: update dependencies

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,12 +1,13 @@
 import * as Router from 'koa-router';
 import { AppConfig } from '../../config';
+import { Context } from 'koa';
 
 export const createApiRoutes = ({ appConfig }: { appConfig: AppConfig }) => {
   const api = new Router({
     prefix: '/api/v1',
   });
 
-  api.get('/config', async (ctx) => {
+  api.get('/config', async (ctx: Context) => {
     return (ctx.body = {
       app: appConfig,
     });
